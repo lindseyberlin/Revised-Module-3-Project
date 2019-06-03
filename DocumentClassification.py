669d1e514ad87bde2f01c80b0c1b7fb258bc9571
Lindsey Berlin <lindsey.berlin@flatironschool.com>
2019-06-03 18:11:21 -0500

    Improved DocumentClassification class after finding bugs

--- DocumentClassification.py
+++ DocumentClassification.py
@@ -30,9 +30,28 @@ divide by 0
 '''
 import numpy as np
 import pandas as pd
+import time
+
+stop_words = {'ourselves', 'hers', 'between', 'yourself', 'but', 'again',
+              'there', 'about', 'once', 'during', 'out', 'very', 'having',
+              'with', 'they', 'own', 'an', 'be', 'some', 'for', 'do', 'its',
+              'yours', 'such', 'into', 'of', 'most', 'itself', 'other', 'off',
+              'is', 's', 'am', 'or', 'who', 'as', 'from', 'him', 'each',
+              'the', 'themselves', 'until', 'below', 'are', 'we', 'these',
+              'your', 'his', 'through', 'don', 'nor', 'me', 'were', 'her',
+              'more', 'himself', 'this', 'down', 'should', 'our', 'their',
+              'while', 'above', 'both', 'up', 'to', 'ours', 'had', 'she',
+              'all', 'no', 'when', 'at', 'any', 'before', 'them', 'same',
+              'and', 'been', 'have', 'in', 'will', 'on', 'does', 'yourselves',
+              'then', 'that', 'because', 'what', 'over', 'why', 'so', 'can',
+              'did', 'not', 'now', 'under', 'he', 'you', 'herself', 'has',
+              'just', 'where', 'too', 'only', 'myself', 'which', 'those',
+              'i', 'after', 'few', 'whom', 't', 'being', 'if', 'theirs',
+              'my', 'against', 'a', 'by', 'doing', 'it', 'how', 'further',
+              'was', 'here', 'than'}
 
 class DocumentClassification():
-    def __init__(self, data, word_loc, category_loc):
+    def __init__(self, data, word_loc, category_loc, stopwords=False):
         
         # Expects data is a pandas dataframe
         self.data = data
@@ -44,11 +63,18 @@ class DocumentClassification():
         # The location of the target variable, category
         # Expects category is found as a column in the df
         self.category_loc = category_loc
+        self.stopwords = stopwords
+
+        self.p_categories = self.get_prob_categories()
+        self.V = self.len_vocabulary()
+        self.word_freqs = self.calc_word_freqs()
+
+        
 
 
     # Calculating P(Category): probability that a document falls in each category
     # Returns a dictionary with categories as keys
-    def p_categories(self):
+    def get_prob_categories(self):
 
         p_category = dict(self.data[self.category_loc].value_counts(normalize = True))
         return p_category
@@ -75,19 +101,14 @@ class DocumentClassification():
     # Returns a nested dictionary with categories as keys, where the values 
     # are another dictionary with each word within documents of that category
     # as the keys and the frequency of each word as values
-    def word_freqs(self):
+    def calc_word_freqs(self):
         word_freq = {}
 
         categories = self.data[self.category_loc].unique()
 
-
         # Putting our cats in the bag (nested dictionary)
         for cat in categories:
             temp_df = self.data[self.data[self.category_loc] == cat]
-            
-            # for row in temp_df.index:
-            #     count = self.count_words(temp_df[self.word_loc][row])
-            #     word_freq[cat] = count
 
             bag = {}
             for row in temp_df.index:
@@ -95,6 +116,12 @@ class DocumentClassification():
                 for word in doc.split():
                     bag[word] = bag.get(word, 0) + 1
             word_freq[cat] = bag
+
+        if self.stopwords == True:
+            list_keys = list(word_freq.keys())
+            for key in list_keys:
+                if key in stop_words:
+                    del word_freq[key]
         
         return word_freq
 
@@ -110,6 +137,9 @@ class DocumentClassification():
         for text in self.data[self.word_loc]:
             for word in text.split():
                 vocabulary.add(word)
+
+        if self.stopwords:
+            vocabulary = [word for word in vocabulary if word not in stop_words]
 
         # Arriving at our number of unique words
         V = len(vocabulary)
@@ -128,37 +158,40 @@ class DocumentClassification():
     # return_posteriors: boolean, saying whether or not you want to print the values
     #      in the posteriors list (per category)
     def classify_doc(self, doc_content, return_posteriors=False):
-        
+
         # Using count_words function to count the words within the provided doc
         count = self.count_words(doc_content)
-        word_freq = self.word_freqs()
-        p_category = self.p_categories()
-        V = self.len_vocabulary()
-        
+
+        if self.stopwords:
+            list_count = list(count.keys())
+            for key in list_count:
+                if key in stop_words:
+                    del count[key]
+
         categories = []
         posteriors = []
-        
+
         # This part of our function is putting together the pieces to calculate
         # P(Word | Category):
         
         # The keys of our word_freq dictionary are the categories
         # In other words, we do this one category at a time
-        for category in word_freq.keys():
+        for category in self.word_freqs.keys():
             
             # Finding the default/original probability of that category
             # Here you can see us using the log, rather than the raw probability
-            p = np.log(p_category[category])
+            p = np.log(self.p_categories[category])
             
             # The keys of our count dictionary are the words within the document
             for word in count.keys():
-                
+
                 # Numerator for P(Word|Category): Word Frequency in Doc + 1
                 num = count[word] + 1
                 
                 # Denominator for P(Word|Category): Word Frequency Across 
                 # All Docs in that Category + Number of Words Across All Docs
                 # If the word is not yet in the 'word_freq' dictionary, value = 0
-                denom = word_freq[category].get(word, 0) + V
+                denom = self.word_freqs[category].get(word, 0) + self.V
                 
                 # Adding the probability of that word being in the doc based on 
                 # category to the probability of the category, in order to arrive
@@ -179,5 +212,14 @@ class DocumentClassification():
         if return_posteriors:
             print(posteriors)
         
-        # Returning the category with the highest probability
+        # Returning the category with the highest probability, for doc_content
         return categories[np.argmax(posteriors)]
+
+    def check_accuracy(self):
+        predicted_target = []
+        for row in range(len(self.data)):
+            predicted_target.append(self.classify_doc(self.data[self.word_loc][row]))
+        actual_target = list(self.data[self.category_loc])
+
+        residuals = predicted_target == actual_target
+        return residuals.value_counts(normalize=True)
